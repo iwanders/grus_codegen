2739e5416406e109bb008d03e4ad880ab1450fdd
Ivor Wanders <ivor@iwanders.net>
2025-01-31 20:09:11 -0500

    Loading a file and parsing the commands.

--- grus_codegen/src/clif_support.rs
+++ grus_codegen/src/clif_support.rs
@@ -10,13 +10,25 @@
 */
 
 use anyhow::{Context, Result};
+use cranelift_reader::TestFile;
+
+pub fn process_test_file(test_file: &TestFile) -> Result<()> {
+    // First, compile all functions.
+
+    for (f, detail) in test_file.functions.iter() {
+        for c in detail.comments.iter() {
+            let z = cranelift_reader::parse_run_command(c.text, &f.signature)?;
+            println!("z: {z:?}");
+        }
+    }
+    Ok(())
+}
 
 pub fn test_files<P: AsRef<std::path::Path> + std::fmt::Debug>(files: &[P]) -> Result<()> {
     for f in files {
-        // let p: std::path::PathBuf = f.into();
         let f = std::fs::read_to_string(&f).context(format!("failed to open {f:?}"))?;
-        //let fun = cranelift_reader::parse_test(&f)?;
-        //println!("fun: {fun:?}");
+        let test_file = cranelift_reader::parse_test(&f, Default::default())?;
+        process_test_file(&test_file)?;
     }
-    todo!()
+    Ok(())
 }
