22ea018f84830995a0231e9508ff5a29f10945c3
Ivor Wanders <ivor@iwanders.net>
2025-04-05 13:28:56 -0400

    Add the if statements to the incremental test.

--- grus_codegen/tests/test_incremental.rs
+++ grus_codegen/tests/test_incremental.rs
@@ -12,6 +12,8 @@ fn test_incremental() -> Result<(), anyhow::Error> {
         "0003_more_instructions.clif",
         "0004_arithmetic_sub_i64_i32_i16_i8.clif",
         "0005_arithmetic_mul_i64_i32_i16_i8.clif",
+        "0006b_branch_diverge.clif",
+        "0006c_branch_diverge_simpler.clif",
     ];
 
     let test_settings = grus_codegen::clif_support::TestSettings {
